89a9abb8168de9310571f590dcaf1a7378a666a2
Abhradip Seth <abhradipsethofficial06@gmail.com>
2025-11-08 19:05:24 +0530

    Chnaged Port to 5050 from 5000

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ require('dotenv').config()
 const connectDB = require('./config/db')
 const express = require('express');
 const app= express()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 connectDB()
 
 app.use(express.json())
 const urlRoutes = require('./routes/urls');
-app.use('/api',url)
+app.use('/api',urlRoutes)
 
 app.get('/',(req,res)=>{
     res.send("API is running...")
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT,'0.0.0.0',()=>{
     console.log(`Server is running on port ${PORT}`)
 })
