9396ad310c4161fc6f44b3b30715b382dca8f555
Abhradip Seth <abhradipsethofficial06@gmail.com>
2025-11-15 21:51:12 +0530

    Changed in urlController.js file

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -5,8 +5,17 @@
 
 
 const shortenUrl = async (req,res)=>{
-    res.status(200).json({success:true,message:"ROute is working!!"
-    })
+    const validUrl = require('valid-url');
+    const {longUrl}=req.body
+    console.log("Received Long Url!!")
+    if(!longUrl){
+        return res.status(400).json({ success: false, error: 'Please provide a URL' });
+    }
+    if(!validUrl.isUri(longUrl)){
+        return res.status(400).json({ success: false, error: 'Invalid URL format provided' });
+    }
+    res.status(200).json({ success: true, message: 'Controller is now connected!', data: { receivedUrl: longUrl } });
+    
 }
 
 
